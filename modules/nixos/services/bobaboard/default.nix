8b92d33449b7b18f409022a8f2d7d78b39fe05b7
Essential Randomness <essential.randomn3ss@gmail.com>
2023-07-03 04:30:04 +0000

    init postgres

--- modules/nixos/services/bobaboard/default.nix
+++ modules/nixos/services/bobaboard/default.nix
@@ -36,9 +36,9 @@ in
         host all all ::1/128 trust
       '';
       initialScript = pkgs.writeText "backend-initScript" ''
-        CREATE ROLE nixcloud WITH LOGIN PASSWORD 'nixcloud' CREATEDB;
-        CREATE DATABASE nixcloud;
-        GRANT ALL PRIVILEGES ON DATABASE nixcloud TO nixcloud;
+        CREATE ROLE the_amazing_bobaboard WITH LOGIN PASSWORD 'how_secure_can_this_db_be' CREATEDB;
+        CREATE DATABASE bobaboard_test;
+        GRANT ALL PRIVILEGES ON DATABASE bobaboard_test TO the_amazing_bobaboard;
       '';
   };
   
