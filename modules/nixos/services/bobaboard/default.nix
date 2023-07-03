bb917a668089f98a40af40b8d02b9c58baa8dc02
Essential Randomness <essential.randomn3ss@gmail.com>
2023-07-02 19:45:32 -0700

    add postgres and git

--- modules/nixos/services/bobaboard/default.nix
+++ modules/nixos/services/bobaboard/default.nix
@@ -1,4 +1,4 @@
-{ lib, config, inputs, system, ... }:
+{ lib, config, inputs, system, pkgs, ... }:
 
 let
   cfg = config.services.bobaboard;
@@ -15,6 +15,7 @@ in
     networking.firewall.allowedTCPPorts = [
       4200
     ];
+    
 
     users = {
       users.bobaboard = {
@@ -24,6 +25,22 @@ in
 
       groups.bobaboard = {};
     };
+
+    services.postgresql = {
+      enable = true;
+      package = pkgs.postgresql_12;
+      enableTCPIP = true;
+      authentication = pkgs.lib.mkOverride 10 ''
+        local all all trust
+        host all all 127.0.0.1/32 trust
+        host all all ::1/128 trust
+      '';
+      initialScript = pkgs.writeText "backend-initScript" ''
+        CREATE ROLE nixcloud WITH LOGIN PASSWORD 'nixcloud' CREATEDB;
+        CREATE DATABASE nixcloud;
+        GRANT ALL PRIVILEGES ON DATABASE nixcloud TO nixcloud;
+      '';
+  };
   
     systemd.services.bobabackend = {
       after = [ "network.target" ];
