20ae9346327b3d4938d9558de6469c3e83e27940
Essential Randomness <essential.randomn3ss@gmail.com>
2024-06-06 02:06:48 +0000

    add cachix config

--- flake.nix
+++ flake.nix
@@ -29,6 +29,15 @@
     };
   };
 
+  nixConfig = {
+    extra-substituters = [
+      "https://bobaboard.cachix.org"
+    ];
+    extra-trusted-public-keys = [
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+    ];
+  };
+
   outputs = inputs:
     inputs.snowfall-lib.mkFlake {
       inherit inputs;
